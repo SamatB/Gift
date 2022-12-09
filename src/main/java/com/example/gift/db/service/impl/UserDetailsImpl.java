ebd4bba3cd15cfed5a16191840961a781f4cd9ca
samat <samhardy1927@gmail.com>
2022-12-10 01:46:16 +0300

    UserDetailsImpl

--- src/main/java/com/example/gift/db/service/impl/UserDetailsImpl.java
+++ src/main/java/com/example/gift/db/service/impl/UserDetailsImpl.java
@@ -1,14 +1,12 @@
 package com.example.gift.db.service.impl;
 
 import com.example.gift.db.entity.User;
-import com.example.gift.enums.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
