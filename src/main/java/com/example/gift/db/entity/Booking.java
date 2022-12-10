515a9a4e5c3e2b269bbd9cdb230e3d15626ce8c8
samat <samhardy1927@gmail.com>
2022-12-11 00:06:38 +0300

    Book

--- src/main/java/com/example/gift/db/entity/Booking.java
+++ src/main/java/com/example/gift/db/entity/Booking.java
@@ -1,4 +1,46 @@
 package com.example.gift.db.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "booking")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
+
+    @Id
+    @GeneratedValue(generator = "booking_gen", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "booking_gen", sequenceName = "booking_seq", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User userId;
+
+    @OneToOne
+    @JoinColumn(name = "charity_id")
+    @JsonIgnore
+    private Charity charity;
+
+    @OneToOne
+    @JoinColumn(name = "wish_list_id")
+    @JsonIgnore
+    private Wishlist wishList;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
 }
+
