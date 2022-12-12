548be8ae8c0f14b0464fffa0c4a184864fe1bd76
samat <samhardy1927@gmail.com>
2022-12-13 00:26:23 +0300

    Category

--- src/main/java/com/example/gift/db/entity/Category.java
+++ src/main/java/com/example/gift/db/entity/Category.java
@@ -1,10 +1,13 @@
 package com.example.gift.db.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table
@@ -18,5 +21,15 @@ public class Category {
     @GeneratedValue(generator = "category_gen", strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name = "category_gen", sequenceName = "category_seq", allocationSize = 1)
     private Long id;
+
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    @JsonIgnore
+    private List<Subcategory> subcategories;
+
+    @OneToMany
+    @JsonIgnore
+    private List<Charity> charities;
 
 }
