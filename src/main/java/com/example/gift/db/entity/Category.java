4cb18a05eca27ff36142ac17fb06638584f1b6f0
samat <samhardy1927@gmail.com>
2022-12-13 00:17:02 +0300

    Category

--- src/main/java/com/example/gift/db/entity/Category.java
+++ src/main/java/com/example/gift/db/entity/Category.java
@@ -1,4 +1,17 @@
 package com.example.gift.db.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 }
